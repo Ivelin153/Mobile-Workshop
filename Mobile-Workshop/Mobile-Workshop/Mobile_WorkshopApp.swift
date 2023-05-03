c607705f7ff2dfab72dac265fe0d40c28e751450
Ivelin Todorovv <i.todorov15@gmail.com>
2023-05-03 14:05:34 +0300

    Create empty Swift project

---
+++ Mobile-Workshop/Mobile-Workshop/Mobile_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_WorkshopApp.swift
+//  Mobile-Workshop
+//
+//  Created by iOS Dev on 3.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
